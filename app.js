bad45ccf3f1c5be67dfdeabb58eb95bf06953526
camen-julia <camjuliaa@gmail.com>
2026-07-11 09:50:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,72 +1,88 @@
 import {
-db,
-collection,
-getDocs,
-doc,
-updateDoc,
-increment
+  db,
+  collection,
+  getDocs,
+  doc,
+  updateDoc,
+  increment
 } from "./firebase.js";
 
 const videoList = document.getElementById("videoList");
 
-loadVideos();
+let videos = [];
 
-async function loadVideos(){
+async function loadVideos() {
 
-videoList.innerHTML="";
+  videoList.innerHTML = "";
 
-const snapshot = await getDocs(collection(db,"videos"));
+  const snapshot = await getDocs(collection(db, "videos"));
 
-snapshot.forEach(item=>{
+  videos = [];
 
-const data=item.data();
+  snapshot.forEach(item => {
 
-const id=item.id;
+    videos.push({
+      id: item.id,
+      ...item.data()
+    });
 
-const youtubeId=getYoutubeId(data.videoUrl);
+  });
 
-videoList.innerHTML+=`
+  videos.forEach(video => {
+
+    videoList.innerHTML += createCard(video);
+
+  });
+
+}
+
+function createCard(video) {
+
+  const youtubeId = getYoutubeId(video.videoUrl);
+
+  return `
 
 <div class="video-card">
 
 <iframe
-src="https://www.youtube.com/embed/${youtubeId}?enablejsapi=1"
+src="https://www.youtube.com/embed/${youtubeId}?enablejsapi=1&playsinline=1&rel=0"
+allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 allowfullscreen>
 </iframe>
 
 <div class="overlay">
 
-<h2>${data.title}</h2>
+<h2>${video.title}</h2>
 
 </div>
 
 <div class="actions">
 
-<button onclick="likeVideo('${id}')">
+<button onclick="likeVideo('${video.id}')">
 ❤️
 </button>
 
-<span id="like-${id}">
-${data.likes||0}
+<span id="likes-${video.id}">
+${video.likes || 0}
 </span>
 
-<button onclick="shareVideo('${data.videoUrl}')">
+<button onclick="shareVideo('${video.videoUrl}')">
 🔗
 </button>
 
-<button>
+<button disabled>
 👁️
 </button>
 
-<span id="view-${id}">
-${data.views||0}
+<span id="views-${video.id}">
+${video.views || 0}
 </span>
 
 </div>
 
 <button
 class="watch-btn"
-onclick="watchVideo('${id}','${data.videoUrl}')">
+onclick="watchVideo('${video.id}','${video.videoUrl}')">
 
 Watch
 
@@ -76,48 +92,72 @@ Watch
 
 `;
 
-});
-
 }
 
 function getYoutubeId(url){
 
-const match=url.match(/[?&]v=([^&]+)/);
+  if(url.includes("watch?v=")){
 
-if(match) return match[1];
+      return url.split("watch?v=")[1].split("&")[0];
 
-return url.split("/").pop();
+  }
 
-}
+  if(url.includes("youtu.be/")){
 
-window.shareVideo=function(url){
+      return url.split("youtu.be/")[1].split("?")[0];
 
-navigator.clipboard.writeText(url);
+  }
 
-alert("Copied");
-
-}
-
-window.likeVideo=async function(id){
-
-await updateDoc(doc(db,"videos",id),{
-
-likes:increment(1)
-
-});
-
-document.getElementById("like-"+id).innerText++;
+  return "";
 
 }
 
-window.watchVideo=async function(id,url){
+window.likeVideo = async function(id){
 
-await updateDoc(doc(db,"videos",id),{
+  await updateDoc(doc(db,"videos",id),{
 
-views:increment(1)
+      likes:increment(1)
 
-});
+  });
 
-location.href="watch.html?url="+encodeURIComponent(url);
+  const txt=document.getElementById("likes-"+id);
+
+  txt.innerText=parseInt(txt.innerText)+1;
 
 }
+
+window.watchVideo = async function(id,url){
+
+  await updateDoc(doc(db,"videos",id),{
+
+      views:increment(1)
+
+  });
+
+  location.href="watch.html?url="+encodeURIComponent(url);
+
+}
+
+window.shareVideo = async function(url){
+
+  if(navigator.share){
+
+      await navigator.share({
+
+          title:"Videos",
+
+          url:url
+
+      });
+
+  }else{
+
+      await navigator.clipboard.writeText(url);
+
+      alert("Video link copied");
+
+  }
+
+}
+
+loadVideos();
